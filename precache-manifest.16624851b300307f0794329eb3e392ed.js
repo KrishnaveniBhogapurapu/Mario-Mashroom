14003ba0e16d7c9adf7aecc43b4c214770c2a438
krishnavei <krishnaveni.b@cawstudios.com>
2020-07-20 12:36:29 +0530

    Updates

---
+++ precache-manifest.16624851b300307f0794329eb3e392ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0ed822d42faf4e31924a66d10ce7c3",
+    "url": "/Mario-Mashroom/index.html"
+  },
+  {
+    "revision": "c55e3ffc0321405d56f0",
+    "url": "/Mario-Mashroom/static/css/main.686d6d08.chunk.css"
+  },
+  {
+    "revision": "30842fcd86235d3b8b0c",
+    "url": "/Mario-Mashroom/static/js/2.6c3d75c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mario-Mashroom/static/js/2.6c3d75c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55e3ffc0321405d56f0",
+    "url": "/Mario-Mashroom/static/js/main.4271dc1d.chunk.js"
+  },
+  {
+    "revision": "d0e640cc3e9a65aaf616",
+    "url": "/Mario-Mashroom/static/js/runtime-main.1452c538.js"
+  }
+]);
